34fec3525b49ba3a64076b143e2b53843cf9062d
Alexander <hello@alexander.works>
2021-01-26 19:27:54 +0200

    Basic animations with sin() and cos()

---
+++ 06-animations/src/script.js
@@ -0,0 +1,51 @@
+import './style.css';
+import * as THREE from 'three';
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+// Clock
+const clock = new THREE.Clock();
+
+// Animations
+const tick = () => {
+  //   console.log('tick!');
+  const elapsedTime = clock.getElapsedTime();
+
+  mesh.position.y = Math.sin(elapsedTime * 2);
+  mesh.position.x = Math.cos(elapsedTime * 2);
+  //   mesh.scale.set(Math.sin(elapsedTime * 2));
+  //   mesh.scale.x = Math.sin(elapsedTime) / 2 + 1;
+
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(tick);
+};
+
+tick();
