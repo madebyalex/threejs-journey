145c8a33c414385468c614a1f9340c4f83518719
Alexander <hello@alexander.works>
2021-01-28 01:59:48 +0200

    Make the scene able to go to full-screen mode

--- 08-fullscreen-and-resizing/src/script.js
+++ 08-fullscreen-and-resizing/src/script.js
@@ -45,6 +45,29 @@ window.addEventListener('resize', () => {
 });
 
 /**
+ * Full screen
+ */
+
+window.addEventListener('dblclick', () => {
+  const fullscreenElement =
+    document.fullscreenElement || document.webkitFullscreenElement;
+
+  if (!fullscreenElement) {
+    if (canvas.requestFullscreen) {
+      canvas.requestFullscreen();
+    } else if (canvas.webkitRequestFullscreen) {
+      canvas.webkitRequestFullscreen();
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+});
+
+/**
  * Camera
  */
 // Base camera
