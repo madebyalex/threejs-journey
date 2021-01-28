e57b4fe3df45040a372253b4ab956463fc829825
Alexander <hello@alexander.works>
2021-01-28 16:21:10 +0200

    Create custom buffer geometry – Single triangle

--- 09-geometries/src/script.js
+++ 09-geometries/src/script.js
@@ -12,44 +12,73 @@ const canvas = document.querySelector('canvas.webgl');
 const scene = new THREE.Scene();
 
 // Object
-// const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
+const geometry = new THREE.BoxBufferGeometry(1, 1, 1, 2, 2, 2);
 
-const customGeometry = new THREE.Geometry();
+// Custom geometry
+// const customGeometry = new THREE.Geometry();
 
-// const vertex1 = new THREE.Vector3(0, 0, 0);
-// customGeometry.vertices.push(vertex1);
+// // const vertex1 = new THREE.Vector3(0, 0, 0);
+// // customGeometry.vertices.push(vertex1);
 
-// const vertex2 = new THREE.Vector3(0, 1, 0);
-// customGeometry.vertices.push(vertex2);
+// // const vertex2 = new THREE.Vector3(0, 1, 0);
+// // customGeometry.vertices.push(vertex2);
 
-// const vertex3 = new THREE.Vector3(1, 0, 0);
-// customGeometry.vertices.push(vertex3);
+// // const vertex3 = new THREE.Vector3(1, 0, 0);
+// // customGeometry.vertices.push(vertex3);
 
-// const face = new THREE.Face3(0, 1, 2);
-// customGeometry.faces.push(face);
+// // const face = new THREE.Face3(0, 1, 2);
+// // customGeometry.faces.push(face);
 
-for (let i = 0; i < 50; i++) {
-  for (let j = 0; j < 3; j++) {
-    customGeometry.vertices.push(
-      new THREE.Vector3(
-        (Math.random() - 0.5) * 4,
-        (Math.random() - 0.5) * 4,
-        (Math.random() - 0.5) * 4
-      )
-    );
-  }
+// for (let i = 0; i < 50; i++) {
+//   for (let j = 0; j < 3; j++) {
+//     customGeometry.vertices.push(
+//       new THREE.Vector3(
+//         (Math.random() - 0.5) * 4,
+//         (Math.random() - 0.5) * 4,
+//         (Math.random() - 0.5) * 4
+//       )
+//     );
+//   }
 
-  const verticesIndex = i * 3;
-  customGeometry.faces.push(
-    new THREE.Face3(verticesIndex, verticesIndex + 1, verticesIndex * 2)
-  );
-}
+//   const verticesIndex = i * 3;
+//   customGeometry.faces.push(
+//     new THREE.Face3(verticesIndex, verticesIndex + 1, verticesIndex * 2)
+//   );
+
+//   // customGeometry.position.x = (Math.random() - 0.5) * 4;
+//   // customGeometry.position.y = (Math.random() - 0.5) * 4;
+//   // customGeometry.position.z = (Math.random() - 0.5) * 4;
+// }
+
+// Custom Buffer geometry
+
+// Explicit version
+// const positionsArray = new Float32Array(9);
+
+// positionsArray[0] = 0;
+// positionsArray[1] = 0;
+// positionsArray[2] = 0;
+
+// positionsArray[3] = 0;
+// positionsArray[4] = 1;
+// positionsArray[5] = 0;
+
+// positionsArray[6] = 1;
+// positionsArray[7] = 0;
+// positionsArray[8] = 0;
+
+// Shorter version
+const positionsArray = new Float32Array([0, 0, 0, 0, 1, 0, 1, 0, 0]);
+
+const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
+const bufferGeometry = new THREE.BufferGeometry();
+bufferGeometry.setAttribute('position', positionsAttribute);
 
 const material = new THREE.MeshBasicMaterial({
   color: 0xff0000,
   wireframe: true,
 });
-const mesh = new THREE.Mesh(customGeometry, material);
+const mesh = new THREE.Mesh(bufferGeometry, material);
 scene.add(mesh);
 
 // Sizes
