6f23f21d7c7c41afda0707f504d33c34af3fa71c
Alexander <hello@alexander.works>
2021-01-30 02:31:25 +0200

    Loading a single texture

--- 11-textures/src/script.js
+++ 11-textures/src/script.js
@@ -1,21 +1,34 @@
 import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import gsap from 'gsap';
-import * as dat from 'dat.gui';
 
-const parameters = {
-  color: 0xffc200,
-  spin: () => {
-    console.log('Spin it!');
-    gsap.to(mesh.rotation, {
-      duration: 1,
-      y: mesh.rotation.y + Math.PI,
-      //   ease: easeInOut,
-      ease: 'elastic.out(1, 0.6)',
-    });
-  },
+/**
+ * Textures
+ */
+
+// Native JS
+const image = new Image();
+const texture = new THREE.Texture(image);
+
+image.onload = () => {
+  texture.needsUpdate = true;
 };
+image.src = './textures/door/color.jpg';
+
+// Using Texture Loader
+const textureLoader = new THREE.TextureLoader();
+const texture2 = textureLoader.load(
+  './textures/door/color.jpg',
+  () => {
+    console.log('load');
+  },
+  () => {
+    console.log('progress');
+  },
+  () => {
+    console.log('error');
+  }
+);
 
 /**
  * Base
@@ -30,7 +43,7 @@ const scene = new THREE.Scene();
  * Object
  */
 const geometry = new THREE.BoxBufferGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: parameters.color });
+const material = new THREE.MeshBasicMaterial({ map: texture2 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
@@ -66,35 +79,14 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.z = 3;
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 1;
 scene.add(camera);
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
-
-/**
- * Debug
- */
-
-// const gui = new dat.GUI();
-// gui.hide();
-// gui.closed = true;
-const gui = new dat.GUI({ closed: true, width: 400 });
-
-// gui.add(mesh.position, 'y', -3, 3, 0.01);
-gui.add(mesh.position, 'y').min(-3).max(3).step(0.01).name('Position Y');
-gui.add(mesh, 'visible');
-gui.add(material, 'wireframe');
-gui.addColor(parameters, 'color').onChange((e) => {
-  //   mesh.material.color = new THREE.Color(e);
-  //   console.log(new THREE.Color(e));
-  //   material.color.set(e);
-  material.color.set(parameters.color);
-});
-gui.add(parameters, 'spin');
-
-// console.log(mesh.material.wireframe);
 
 /**
  * Renderer
