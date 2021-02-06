df1b3ba5d7e71311f57ea17e92e8d58a6092e333
Alexander <hello@alexander.works>
2021-02-06 17:38:40 +0200

    Shadows: Point light

--- 15-shadows/src/script.js
+++ 15-shadows/src/script.js
@@ -20,12 +20,12 @@ const scene = new THREE.Scene();
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
 gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001);
 scene.add(ambientLight);
 
 // Directional light
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.4);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3);
 directionalLight.position.set(2, 2, -1);
 gui.add(directionalLight, 'intensity').min(0).max(1).step(0.001);
 gui.add(directionalLight.position, 'x').min(-5).max(5).step(0.001);
@@ -51,7 +51,7 @@ directionalLightCameraHelper.visible = false;
 scene.add(directionalLightCameraHelper);
 
 // Spot light
-const spotLight = new THREE.SpotLight(0xffffff, 0.4, 10, Math.PI * 0.3);
+const spotLight = new THREE.SpotLight(0xffffff, 0.3, 10, Math.PI * 0.3);
 spotLight.castShadow = true;
 spotLight.shadow.mapSize.width = 1024;
 spotLight.shadow.mapSize.height = 1024;
@@ -66,6 +66,22 @@ scene.add(spotLight.target);
 const spotLightCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera);
 spotLightCameraHelper.visible = false;
 scene.add(spotLightCameraHelper);
+
+// Point light
+const pointLight = new THREE.PointLight(0xffffff, 0.3);
+pointLight.castShadow = true;
+
+pointLight.shadow.mapSize.width = 1024;
+pointLight.shadow.mapSize.height = 1024;
+pointLight.shadow.camera.near = 0.1;
+pointLight.shadow.camera.far = 5;
+
+pointLight.position.set(-1, 1, 0);
+scene.add(pointLight);
+
+const pointLightCameraHelper = new THREE.CameraHelper(pointLight.shadow.camera);
+pointLightCameraHelper.visible = false;
+scene.add(pointLightCameraHelper);
 
 /**
  * Materials
