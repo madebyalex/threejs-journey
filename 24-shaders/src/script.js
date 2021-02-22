b8c52c3a76821a6413ce6f3504df6b9c4262e2a4
Alexander <hello@alexander.works>
2021-02-23 01:01:51 +0200

    Shaders: Put shader parts into separate files

--- 24-shaders/src/script.js
+++ 24-shaders/src/script.js
@@ -2,6 +2,8 @@ import './style.css';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'dat.gui';
+import testVertexShader from './shaders/test/vertex.glsl';
+import testFragmentShader from './shaders/test/fragment.glsl';
 
 /**
  * Base
@@ -28,23 +30,10 @@ const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
 
 // Material
 const material = new THREE.RawShaderMaterial({
-  vertexShader: `
-    uniform mat4 projectionMatrix;
-    uniform mat4 viewMatrix;
-    uniform mat4 modelMatrix;
-
-    attribute vec3 position;
-
-    void main() {
-        gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
-    }
-  `,
-  fragmentShader: `
-    precision mediump float;
-    void main() {
-        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-    }
-  `,
+  vertexShader: testVertexShader,
+  fragmentShader: testFragmentShader,
+  //   wireframe: true,
+  //   side: THREE.DoubleSide,
 });
 
 // Mesh
