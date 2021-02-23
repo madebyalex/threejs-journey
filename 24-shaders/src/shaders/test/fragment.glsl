506f113cf0b37395f295d8c0fa118611405b7cfc
Alexander <hello@alexander.works>
2021-02-23 23:51:36 +0200

    Shaders: Apply shadows to the flag

--- 24-shaders/src/shaders/test/fragment.glsl
+++ 24-shaders/src/shaders/test/fragment.glsl
@@ -4,9 +4,11 @@ uniform vec3 uColor;
 uniform sampler2D uTexture;
 
 varying vec2 vUv;
+varying float vElevation;
 
 void main() {
     vec4 textureColor = texture2D(uTexture, vUv);
+    textureColor.rgb *= vElevation * 1.6 + 0.6;
 
     // gl_FragColor = vec4(1.0, 0.5, 0.0, 1);
     // gl_FragColor = vec4(uColor, 1);
--- 24-shaders/src/shaders/test/vertex.glsl
+++ 24-shaders/src/shaders/test/vertex.glsl
@@ -8,19 +8,25 @@ attribute vec3 position;
 attribute vec2 uv;
 
 varying vec2 vUv;
+varying float vElevation;
 
 void main() {
   
   // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-  modelPosition.z += sin(modelPosition.x * uFrequency.x - (uTime * 3.0)) * 0.1;
-  modelPosition.z += sin(modelPosition.y * uFrequency.y) * 0.1;
+
+  float elevation = sin(modelPosition.x * uFrequency.x - (uTime * 3.0)) * 0.1;
+  elevation += sin(modelPosition.y * uFrequency.y) * 0.1;
+
+  modelPosition.z += elevation;
 
   vec4 viewPosition = viewMatrix * modelPosition;
   vec4 projectedPosition = projectionMatrix * viewPosition;
 
   gl_Position = projectedPosition;
 
+  // Passing varyings to the fragment shader
   vUv = uv;
+  vElevation = elevation;
 }
