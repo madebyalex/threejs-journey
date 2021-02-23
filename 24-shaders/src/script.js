ffd0f7814beae943982101593414517479c4df59
Alexander <hello@alexander.works>
2021-02-23 02:24:17 +0200

    Shaders: Set up new custom attribute and pass it to the shader

--- 24-shaders/src/script.js
+++ 24-shaders/src/script.js
@@ -28,12 +28,22 @@ const textureLoader = new THREE.TextureLoader();
 // Geometry
 const geometry = new THREE.PlaneGeometry(1, 1, 32, 32);
 
+const count = geometry.attributes.position.count;
+const randoms = new Float32Array(count);
+
+for (let i = 0; i < count; i++) {
+  randoms[i] = Math.random();
+}
+
+geometry.setAttribute('aRandom', new THREE.BufferAttribute(randoms, 1));
+
 // Material
 const material = new THREE.RawShaderMaterial({
   vertexShader: testVertexShader,
   fragmentShader: testFragmentShader,
   //   wireframe: true,
   side: THREE.DoubleSide,
+  transparent: true,
 });
 
 // Mesh
--- 24-shaders/src/shaders/test/vertex.glsl
+++ 24-shaders/src/shaders/test/vertex.glsl
@@ -3,13 +3,14 @@ uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
 
 attribute vec3 position;
+attribute float aRandom;
 
 void main() {
   
   // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-  modelPosition.z += sin(modelPosition.x * 20.0) * 0.05;
+  modelPosition.z += sin(modelPosition.x * 10.0) * 0.05 + aRandom * 0.05;
 
   vec4 viewPosition = viewMatrix * modelPosition;
   vec4 projectedPosition = projectionMatrix * viewPosition;
