877f358e86b7347ae950800520ae95d0b7dd8df2
Alexander <hello@alexander.works>
2021-02-27 02:20:05 +0200

    Shader patterns: Part 2

--- 25-shader-patterns/src/shaders/test/fragment.glsl
+++ 25-shader-patterns/src/shaders/test/fragment.glsl
@@ -34,15 +34,99 @@ void main()
     // strength = step(0.8, strength); // Black & white stripes, with wider black stripes
 
     // Pattern 10
-    float strength = mod(vUv.x * 10.0, 1.0);
-    strength = step(0.8, strength); // Black & white stripes, with wider black stripes, left to right
+    // float strength = mod(vUv.x * 10.0, 1.0);
+    // strength = step(0.8, strength); // Black & white stripes, with wider black stripes, left to right
 
     // Pattern 10-2
-    float strengthX = mod(vUv.x * 10.0, 1.0);
-    strengthX = step(0.8, strengthX);
+    // float strengthX = mod(vUv.x * 10.0, 1.0);
+    // strengthX = step(0.8, strengthX);
 
-    float strengthY = mod(vUv.y * 10.0, 1.0);
-    strengthY = step(0.8, strengthY); // Green / purple grid
+    // float strengthY = mod(vUv.y * 10.0, 1.0);
+    // strengthY = step(0.8, strengthY); // Green / purple grid
 
-    gl_FragColor = vec4(strengthX, strengthY, strengthX, 1.0);
+    // Pattern 11
+    // float strength = step(0.8, mod(vUv.x * 10.0, 1.0));
+    // strength += step(0.8, mod(vUv.y * 10.0, 1.0)); // White grid
+
+    // Pattern 12
+    // float strength = step(0.8, mod(vUv.x * 10.0, 1.0));
+    // strength *= step(0.8, mod(vUv.y * 10.0, 1.0)); // Dotted grid, white dots on black
+
+    // Pattern 13
+    // float strength = step(0.4, mod(vUv.x * 10.0, 1.0));
+    // strength *= step(0.8, mod(vUv.y * 10.0, 1.0)); // White dashes grid
+
+    // Pattern 14
+    // float barX = step(0.4, mod(vUv.x * 10.0, 1.0));
+    // barX *= step(0.8, mod(vUv.y * 10.0, 1.0));
+
+    // float barY = step(0.8, mod(vUv.x * 10.0, 1.0));
+    // barY *= step(0.4, mod(vUv.y * 10.0, 1.0));
+
+    // float strength = barX + barY; // White angles
+
+    // Pattern 14-2
+    // float barX = step(0.4, mod(vUv.x * 10.0, 1.0));
+    // barX *= step(0.8, mod(vUv.y * 10.0, 1.0));
+
+    // float barY = step(0.8, mod(vUv.x * 5.0, 1.0));
+    // barY *= step(0.4, mod(vUv.y * 5.0, 1.0));
+
+    // float strength = barX + barY; // S-like patterns
+
+    // Pattern 15
+    // float barX = step(0.4, mod(vUv.x * 10.0, 1.0));
+    // barX *= step(0.8, mod(vUv.y * 10.0, 1.0));
+
+    // float barY = step(0.8, mod(vUv.x * 10.0 + 0.2, 1.0));
+    // barY *= step(0.4, mod(vUv.y * 10.0 - 0.2, 1.0));
+
+    // float strength = barX + barY; // White plus signs
+
+    // Pattern 15-2
+    // float strength = vUv.x - 0.5;
+    // strength *= vUv.y - 0.5; // Highlighted corners with radial white gradients
+
+    // Pattern 16
+    // float strength = abs(vUv.x - 0.5);
+
+    // Pattern 16-2
+    // float strength = abs(vUv.x - 0.5);
+    // strength += abs(vUv.y - 0.5); // Black star
+
+    // Pattern 17
+    // float strength = min(abs(vUv.x - 0.5), abs(vUv.y - 0.5)); // Double angle gradients
+
+    // Pattern 18
+    // float strength = max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)); // Diagonal dark star
+
+    // Pattern 18-2
+    // float strength = min(abs(vUv.x - 0.5), abs(vUv.y - 0.5)) / max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)); // Bright double angle gradients
+
+    // Pattern 19
+    // float strength = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5))); // Black square in the center
+
+    // Pattern 19-2
+    // float strength = step(0.05, min(abs(vUv.x - 0.5), abs(vUv.y - 0.5))); // Window-like cross
+
+    // Pattern 19-3
+    // float strength = step(0.4, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5))); // Wide white frame
+
+    // Pattern 20
+    // float square1 = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+    // float square2 = 1.0 - step(0.25, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+    // float strength = square1 * square2; // White frame inside a black square
+
+    // Pattern 21
+    // float strength = floor(vUv.x * 10.0) / 10.0; // Stepped gradient
+
+    // Pattern 21-2
+    // float strength = floor(vUv.x * 10.0) / 10.0;
+    // strength *= floor(vUv.y * 10.0) / 10.0; // Stepped gradient with a grid
+
+    // Pattern 21-3
+    float strength = floor(vUv.x * 10.0) / 10.0;
+    strength += floor(vUv.y * 10.0) / 10.0; // Angled stepped gradient with a grid
+
+    gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
