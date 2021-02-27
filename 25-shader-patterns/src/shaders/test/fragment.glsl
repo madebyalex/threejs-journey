44d79688e7f228def7778b7ba7890fb6713462ad
Alexander <hello@alexander.works>
2021-02-27 18:49:37 +0200

    Shader patterns: Part 5

--- 25-shader-patterns/src/shaders/test/fragment.glsl
+++ 25-shader-patterns/src/shaders/test/fragment.glsl
@@ -1,8 +1,16 @@
+#define PI 3.1415926535897932384626433832795
+
 varying vec2 vUv;
 
-float random(vec2 st)
-{
+float random(vec2 st) {
     return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+}
+
+vec2 rotate(vec2 uv, float rotation, vec2 mid) {
+    return vec2(
+      cos(rotation) * (uv.x - mid.x) + sin(rotation) * (uv.y - mid.y) + mid.x,
+      cos(rotation) * (uv.y - mid.y) - sin(rotation) * (uv.x - mid.x) + mid.y
+    );
 }
 
 void main()
@@ -155,7 +163,37 @@ void main()
     // float strength = distance(vUv, vec2(0.5)); // Dark radial gradient at the center, v2
 
     // Pattern 28
-    float strength = 1.0 - distance(vUv, vec2(0.5)); // Ligth radial gradient at the center
+    // float strength = 1.0 - distance(vUv, vec2(0.5)); // Ligth radial gradient at the center
+
+    // Pattern 29
+    // float strength = 0.015 /  distance(vUv, vec2(0.5)); // Bright small radial gradient at the center
+
+    // Pattern 30
+    // vec2 lightUv = vec2(
+    //     vUv.x * 0.1 + 0.45,
+    //     vUv.y * 0.5 + 0.25
+    // );
+    // float strength = 0.015 /  distance(lightUv, vec2(0.5)); // Stretched radial gradient at the center
+
+    // Pattern 31
+    // vec2 lightUvX = vec2(vUv.x * 0.1 + 0.45, vUv.y * 0.5 + 0.25);
+    // float lightX = 0.015 /  distance(lightUvX, vec2(0.5));
+    
+    // vec2 lightUvY = vec2(vUv.y * 0.1 + 0.45, vUv.x * 0.5 + 0.25);
+    // float lightY = 0.015 /  distance(lightUvY, vec2(0.5));
+
+    // float strength = lightX * lightY; // Centered bright star
+
+    // Pattern 32
+    vec2 rotatedUv = rotate(vUv, PI * 0.25, vec2(0.5));
+
+    vec2 lightUvX = vec2(rotatedUv.x * 0.1 + 0.45, rotatedUv.y * 0.5 + 0.25);
+    float lightX = 0.015 /  distance(lightUvX, vec2(0.5));
+
+    vec2 lightUvY = vec2(rotatedUv.y * 0.1 + 0.45, rotatedUv.x * 0.5 + 0.25);
+    float lightY = 0.015 /  distance(lightUvY, vec2(0.5));
+
+    float strength = lightX * lightY; // Centered bright star
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
