79c4f20b264e656d6cb0db488791521eedb063ab
Alexander <hello@alexander.works>
2021-03-04 03:25:04 +0200

    Animated shader galaxy: :tada:  Init

---
+++ 27-animated-galaxy/src/shaders/galaxy/vertex.glsl
@@ -0,0 +1,16 @@
+uniform float uSize;
+
+void main() {
+    /**
+    * Position
+    */
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+    gl_Position = projectedPosition;
+
+    /**
+    * Size
+    */
+    gl_PointSize = uSize;
+}
